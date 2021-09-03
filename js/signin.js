9c8f926e872256066d71036be6f6c7b076eb84a2
AKHIL JP <akhilakku008@gmail.com>
2021-09-03 22:52:30 +0530

    login/signup validated2

--- js/signin.js
+++ js/signin.js
@@ -1,82 +1,227 @@
 var email = document.getElementById("email")
 var emaillab = document.getElementById("emaillabel")
-var password = document.getElementById("pass")
+var password = document.getElementById("TextBox1")
 var passlab = document.getElementById("passlabel")
 
 
-email.addEventListener('input',function (){
+email.onfocus = function() {
+  emaillab.innerHTML ="Enter your email address"
+  emaillab.style.width= "220px"
+}
+// When the user clicks outside of the password field, hide the message box
+email.onblur = function() {
+  emaillabel.innerHTML =""
+
+}
+
+
+// email.addEventListener('input',emailValid)
+function emailValid()
+{
 // function validate(){
     // var regex = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/   **seems some runtime bug
     var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    if(email.value == ""){
+    if(email.value ==""){
         emaillab.innerText = "Email cannot be empty"
         emaillab.style.backgroundColor = "brown"
         emaillab.style.width = "200px" 
+        email.style.borderColor = "red"
     
         
         return false;
     }
     else if(regex.test(email.value)==false){
-        emaillab.innerText = "Must be a valid email"
-        emaillab.style.width = "200px"
-        emaillab.style.backgroundColor = "red"
+        emaillab.innerText = "Please enter a valid email❕"
+        emaillab.style.width = "225px"
+        emaillab.style.backgroundColor = "rgba(92, 57, 0)"
+        email.style.borderColor = "red"
         return false;
     }
     else{
-        emaillab.innerText = "Your Email Address is Valid"
-        emaillab.style.width = "230px"
+        emaillab.innerText = "Email is Validated"
+        emaillab.style.width = "150px"
         emaillab.style.backgroundColor = "green"
+        email.style.borderColor = "green"
         return true;
+        
+        
 
     }
-});
-password.addEventListener('input',function ()
-{   
+};
 
-   
-    var passcase = document.getElementById("passcase")
-    var strongPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
-    var mediumPassword = /((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{2,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))/
-    var submit = document.getElementById("submit")
-   if(password.value == ""){
-       passlab.innerText = "Password cannot be empty";
-       passlab.style.backgroundColor = "brown"
-       passlab.style.width = "225px" 
-       passcase.style.color = "yellow"
-    //    passcase.innerText = "Minimum 8 characters, at least\n one uppercase, and one lower case,\n must contain at least one number";
-       return false;
-   }
-   else if(password.value.match(strongPassword)){
 
-       passlab.style.backgroundColor = "green"
-       passlab.innerText = "Strong";
-    //    passlab.innerText = "Password ";
-       passlab.style.width="180px"
-       passcase.style.color ="cyan"
-       passcase.innerText = "Password succesfully validated";
-       
-      
-       
-   }
-   
- else if(password.value.match(mediumPassword)) {
-    passlab.style.backgroundColor = "orange"
-    passlab.style.width="180px"
-    passlab.innerText = 'Medium'
-    passcase.innerText = "";
-}
+    // var strongPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
+    // var mediumPassword = /((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{2,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))/
+//
 
-else{
-    passlab.style.backgroundColor = 'red'
-    passlab.style.width="180px"
-    passlab.innerText = 'Poor'
-    // passcase.innerText = "One uppercase, and one lower case,\n must contain at least one number"
-    // passcase.style.color = "yellow"
-    passcase.innerText = "";
-}
+var passwordTextBox = document.getElementById("psw");
+
+
+var togglePassword = document.querySelector('#togglePassword');
+var password = document.querySelector('#psw');
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type1 = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type1);
+  // toggle the eye / eye slash icon
+  this.classList.toggle('bi-eye');
 });
 
 
 
+
+//new srengthCode 01/09/2021
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+  document.getElementById("Progress").style.visibility = "visible";
+  passlab.innerHTML = "Enter your Password"
+  passlab.style.width ="200px"
+  passlab.style.backgroundColor ="rgb(92, 57, 0)"
+  
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+  document.getElementById("Progress").style.visibility = "hidden";
+  
+  passlab.innerHTML = ""
+
+}
+
+
+
+
+// When the user starts to type something inside the password field
+// myInput.onkeyup = function() {
+  function passField(){
+  var passwordScore = 0;
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  // if(myInput.value.trim()==""){
+  //   return false;
+  // }
+  if(myInput.value.match(lowerCaseLetters)) {  
+    // letter.classList.remove("invalid");
+    // letter.classList.add("valid");
+    passwordScore += 20;
+    
+  } else {
+    // letter.classList.remove("valid");
+    // letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    // capital.classList.remove("invalid");
+    // capital.classList.add("valid");
+    passwordScore += 20;
+  } else {
+    // capital.classList.remove("valid");
+    // capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    // number.classList.remove("invalid");
+    // number.classList.add("valid");
+    passwordScore += 20;
+    
+  } else {
+    // number.classList.remove("valid");
+    // number.classList.add("invalid");
+    
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    // length.classList.remove("invalid");
+    // length.classList.add("valid");
+    passwordScore += 20;
+  } else {
+    // length.classList.remove("valid");
+    // length.classList.add("invalid");
+    
+  }
+
+
+
+var strength = "";
+// var backgroundColor = "red";
+
+if (passwordScore >= 80) 
+{
+  strength = "Strong";
+  // backgroundColor = "green";
+  passlab.style.backgroundColor = "white"
+  passlab.innerHTML = ""
+  passlab.style.color = "black"
+  myInput.style.borderColor = "green"
+
+  
+
+}
+else if (passwordScore >= 40) 
+{
+  strength = "Medium";
+  // backgroundColor = "Orange";
+  passlab.style.backgroundColor = "white"
+  passlab.style.color = "black"
+  myInput.style.borderColor = "red"
+
+
+}
+else if (passwordScore >= 20) 
+{
+  strength = "Weak";
+  passlab.style.backgroundColor = "white"
+  passlab.style.color = "black"
+  // backgroundColor = "maroon";
+  myInput.style.borderColor = "red"
+  passlab.innerHTML ="please enter a valid email"
+
+}
+else 
+{
+  strength = "Password cannot be empty";
+  backgroundColor = ""
+  passlab.style.backgroundColor = "brown"
+  passlab.style.width= "230px"
+  passlab.style.color = "white"
+  myInput.style.borderColor = "red"
+
+//          
+  
+}   
+
+
+document.getElementById("passlabel").innerHTML = strength;
+passwordTextBox.style.backgroundColor = backgroundColor;
+}
+
+
+function validate(){
+  if(emailValid() && passField()){
+      return true;
+  }else{
+    return false
+  }
+}            
+
+// 
+ 
+         
+       
 
 
--- js/signup.js
+++ js/signup.js
@@ -1,90 +1,339 @@
+//usernamre field
+let user = document.getElementById("username")
+let userlabel = document.getElementById("userlabel")
+user.onfocus = function(){
+     userlabel.innerHTML ="Enter Your User name⚠️";
+     userlabel.style.width ="220px"
+}
+user.onblur = function(){
+  userlabel.innerHTML = ""
+}
+user.onkeyup = userName;
+function userName(){
+  if(user.value.trim()==""){
+    userlabel.innerHTML= "fields cannot be empty"
+    user.style.borderColor="red"
+    userlabel.style.width = "200px"
+    return false;
+  }
+  else{
+    userlabel.innerHTML = "Good to go🏍️.."
+    user.style.borderColor="green"
+    return false;
+  }
+}
+
+
 // signup.js
 var email = document.getElementById("email")
 var emaillab = document.getElementById("emaillabel")
-var password = document.getElementById("pass")
+var password = document.getElementById("TextBox1")
 var passlab = document.getElementById("passlabel")
 
 
-email.addEventListener('input',function (){
-// function validate(){
-    // var regex = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/   **seems some runtime bug
-    var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    if(email.value == ""){
-        emaillab.innerText = "Email cannot be empty"
-        emaillab.style.backgroundColor = "brown"
-        emaillab.style.width = "200px" 
-        
-        return false;
-    }
-    else if(regex.test(email.value)==false){
-        emaillab.innerText = "Must be a valid email"
-        emaillab.style.width = "200px"
-        emaillab.style.backgroundColor = "red"
-        return false;
-    }
-    else{
-        emaillab.innerText = "Your Email Address is Valid"
-        emaillab.style.width = "230px"
-        emaillab.style.backgroundColor = "green"
-        return true;
 
-    }
+
+
+//email
+
+email.onfocus = function() {
+    emaillab.innerHTML ="Enter your email address⚠️"
+    emaillab.style.width= "240px"
+    // emaillab.style.borderColor = "red"
+  }
+  // When the user clicks outside of the password field, hide the message box
+  email.onblur = function() {
+    emaillabel.innerHTML =""
+  
+  }
+  
+  
+  email.onkeyup = emailValid;
+  function emailValid()
+  {
+  // function validate(){
+      // var regex = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/   **seems some runtime bug
+      var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+      if(email.value == ""){
+          emaillab.innerText = "Email cannot be empty"
+          email.style.borderColor ="red"
+          emaillab.style.backgroundColor = "brown"
+          emaillab.style.width = "200px" 
+      
+          
+          return false;
+      }
+      else if(regex.test(email.value)==false){
+          emaillab.innerText = "Please enter a valid email❕"
+          email.style.borderColor ="red"
+          emaillab.style.width = "225px"
+          emaillab.style.backgroundColor = "rgba(92, 57, 0)"
+          return false;
+      }
+      else{
+          emaillab.innerText = "Email is Validated"
+          email.style.borderColor ="green"
+          emaillab.style.width = "150px"
+          emaillab.style.backgroundColor = "green"
+          return true;
+          
+          
+  
+      }
+  };
+
+//email end
+
+
+
+
+
+//password field
+
+var togglePassword = document.querySelector('#togglePassword');
+var password = document.querySelector('#psw');
+var passwordTextBox = document.getElementById("psw");
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type1 = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type1);
+  // toggle the eye / eye slash icon
+  this.classList.toggle('bi-eye');
 });
-password.addEventListener('input',function ()
-{   
 
-   
-    var passcase = document.getElementById("passcase")
-    var strongPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
-    var mediumPassword = /((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{2,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))/
-//   var submit = document.getElementById("submit")
-if(password.value == ""){
-    passlab.innerText = "Password cannot be empty";
-    passlab.style.backgroundColor = "brown"
-    passlab.style.width = "225px" 
-    passcase.style.color = "yellow"
-    passcase.innerText = "Minimum 8 characters, at least\n one uppercase, and one lower case, must contain at least one number"
+
+
+
+//new srengthCode 01/09/2021
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number1 = document.getElementById("number");
+var length = document.getElementById("length");
+// var progressBar = document.getElementById("progressBar")
+
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  // window.scrollTo(240,400);
+  window.scroll({
+    top: 300, 
+    left: 0, 
+    behavior: 'smooth'
+  });
+  document.getElementById("message").style.display = "block";
+  document.getElementById("Progress").style.visibility = "visible";
+  passlab.innerHTML = "Enter your Password⚠️"
+  passlab.style.width ="200px"
+  passlab.style.backgroundColor ="rgb(92, 57, 0)"
+  
+  
+}
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+  document.getElementById("Progress").style.visibility = "hidden";
+
+  
+  passlab.innerHTML = ""
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  var passwordScore = 0;
+  let proPer = document.getElementById("proPer")
+ 
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+    passwordScore += 20;
     
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+    passwordScore += 20;
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number1.classList.remove("invalid");
+    number1.classList.add("valid");
+    passwordScore += 20;
+    
+  } else {
+    number1.classList.remove("valid");
+    number1.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+    passwordScore += 20;
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+
+
+
+var strength = "";
+var backgroundColor = "red";
+
+if (passwordScore >= 80) 
+{
+  strength = "Strong";
+  backgroundColor = "green";
+  passlab.style.backgroundColor = "white"
+  passlab.innerText= ""
+  passlab.style.color = "black"
+  progressBar.style.width="100%"
+  proPer.innerHTML = "100%"
+  progressBar.classList.remove('bg-danger')
+  progressBar.style.backgroundColor = "green"
+  myInput.style.borderColor ="green"
+  
+  window.scrollTo(0,300);
+
+}
+else if (passwordScore >= 40) 
+{
+  strength = "Medium";
+  backgroundColor = "Orange";
+  passlab.style.backgroundColor = "white"
+  passlab.style.color = "black"
+  progressBar.style.width="50%"
+  proPer.innerHTML = "60%"
+  progressBar.classList.remove('bg-danger')
+  progressBar.style.backgroundColor = "orange"
+
+  window.scrollTo(0,300);
+
+}
+else if (passwordScore >= 20) 
+{
+  strength = "Weak";
+  passlab.style.backgroundColor = "white"
+  passlab.style.color = "black"
+  backgroundColor = "maroon";
+  progressBar.style.width="30%"
+  proPer.innerHTML = "20%"
+  progressBar.classList.remove('bg-danger')
+  // progressBar.classList.add('bg')
+  progressBar.style.backgroundColor = "red"
+  passlab.innerHTML ="please enter a valid email"
+  window.scrollTo(0,300);
+ 
+}
+else 
+{
+  strength = "Password cannot be empty";
+  backgroundColor = ""
+  passlab.style.backgroundColor = "brown"
+  passlab.style.width= "230px"
+  passlab.style.color = "white"
+  progressBar.style.backgroundColor = "";
+  progressBar.classList = ""    
+  proPer.innerHTML = "" 
+  myInput.style.borderColor ="red"
+//          
+  
+}   
+
+
+document.getElementById("passlabel").innerHTML = strength;
+passwordTextBox.style.backgroundColor = backgroundColor;
+}
+
+//password END
+var repeat = document.getElementById("pass1")
+var repeatlabel = document.getElementById("passlabel1")
+
+
+function passRepeat(){
+  if(repeat.value.trim()==""){
+    repeat.style.borderColor ="red"
     return false;
-}
-else if(password.value.match(strongPassword)){
-
-    passlab.style.backgroundColor = "green"
-    passlab.innerText = "Strong";
-    passlab.style.width="180px"
-    passcase.innerText = "";
-   
-    
-}
-
-else if(password.value.match(mediumPassword)) {
- passlab.style.backgroundColor = "orange"
- passlab.style.width="180px"
- passlab.innerText = 'Medium'
- passcase.innerText = "One uppercase, and one lower case,\n must contain at least one number"
-//  passcase.style.color = "yellow"
-
+  }
+// document.getElementById("pass1").value == document.getElementById("myInput")
+if(myInput.value == repeat.value){
+  repeatlabel.innerText = "Password success ✔️"
+  repeatlabel.style.width = "180px"
+  repeatlabel.style.backgroundColor ="green"
+  repeat.style.borderColor ="green"
+  return true;
+}else{
+repeatlabel.innerHTML ="Password does not match"
+repeatlabel.style.width = "240px"
+repeatlabel.style.backgroundColor ="rgb(92, 57, 0)"
+repeat.style.borderColor ="red"
+return false;
 }
 
-else{
- passlab.style.backgroundColor = 'red'
- passlab.style.width="180px"
- passlab.innerText = 'Poor'
- passcase.innerText = "One uppercase, and one lower case,\n must contain at least one number"
- passcase.style.color = "yellow"
+// if(repeat.value.trim()==""){
+//   repeatlabel.innerHTML = ""
+// }
 }
-});
+// repeat.onkeydown = function(){
+//   if(repeat.value.trim()==""){
+//     repeat.style.borderColor ="red"
+//     return false;
+//   }
+// }
 
+
+// regex.test(email.value)
+
+
+
+
+
+
+
+//Number Field
 var number = document.getElementById("num");
 var numlabel = document.getElementById("numlabel");
 var numtext = document.getElementById("numtext")
  
-number.addEventListener('input',function(){
+number.onfocus = function(){
+  numlabel.style.backgroundColor = "rgba(92, 57, 0)";
+  numlabel.innerText = "Enter Your Number⚠️";
+  numlabel.style.width = "200px"
+  numtext.innerText = "Accept additional 3 formats:\n eg: XXX-XXX-XXXX, XXX.XXX.XXXX,\n XXX XXX XXXX "
+  numtext.style.color="rgb(91, 133, 33)"
+  window.scrollTo(250,100);
+  
+}
+number.onblur = function(){
+  numtext.innerHTML =""
+  numlabel.innerHTML =""
+}
+
+
+number.onkeyup = numField;
+function numField(){
+  
+// number.addEventListener('input',function(){
     var phoneCode = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     // var phoneCode = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     // var phoneCode = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     if(number.value == ""){
         numlabel.innerText = "Number cannot be empty";
+        number.style.borderColor = "red"
         numlabel.style.backgroundColor = "brown"
         numlabel.style.width = "225px" 
         numtext.innerText = ""
@@ -96,44 +345,25 @@ number.addEventListener('input',function(){
 // XXX-XXX-XXXX
   else if(number.value.match(phoneCode)){
     numlabel.innerText = "Mobile number is valid";
+    number.style.borderColor = "green"
     numlabel.style.width = "200px"  
     numlabel.style.backgroundColor ="green";
     numtext.innerText = "";
     return true;
 }
 
-// else if(number.value.match(phoneRegexp)){   
-//     numlabel.innerText = "Mobile number is valid";
-//     numlabel.style.width = "200px"  
-//     numlabel.style.backgroundColor ="green";
-//     numtext.innerText = "";
-//     return true;
-// }
 else{
-    
+  numlabel.style.backgroundColor = "red";
+  numlabel.innerText = "not a valid number";
+  number.style.borderColor ="red"
+  numtext.innerText = "Accept additional 3 formats:\n XXX-XXX-XXXX, XXX.XXX.XXXX,\n XXX XXX XXXX"
+  numtext.style.color="yellow"
 
-    numlabel.style.backgroundColor = "red";
-    numlabel.innerText = "not a valid number";
-    numtext.innerText = "Accept additional 3 formats:\n XXX-XXX-XXXX, XXX.XXX.XXXX,\n XXX XXX XXXX"
-    numtext.style.color="yellow"
+   
     return false;
 }
-});
-// var number = document.getElementById("num");
-// var numlabel = document.getElementById("numlabel");
-// var phoneCode = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-// number.addEventListener('input',function(){
-// // XXX-XXX-XXXX
-//   if(number.value.value(phoneCode)){
-//     numlabel.style.backgroundColor = "green";
-//     numtext.innerText = "";
-//     numlabel.innerText = "PhoneCode is valid";
-//     return true;
-// }
-
-// });
-
-
+};
+//number field End
 
 
 
